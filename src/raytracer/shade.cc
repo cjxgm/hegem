2a458b6579f082570a3180ff15d32d27932d11d6
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-03-31 00:14:23 +0800

    [+] Prepared for combined pass of shading

--- src/raytracer/shade.cc
+++ src/raytracer/shade.cc
@@ -2,22 +2,111 @@
 #include "../lib/glm/mat3.hh"
 #include "../math/unit.hh"
 #include "../image/color.hh"
+#include "../scene/material.hh"
+#include "../scene/scene.hh"
 #include "shade.hh"
+#include "hit.hh"
 #include <limits>
+#include <stdexcept>
 
 namespace rt::raytracer::shading_details
 {
     namespace
     {
         using direction_type = math::unit<glm::vec3>;
+        using lamp_container_type = scene::scene_type::lamp_container_type;
         using image::color::linear_rgb;
         using image::color::gamma_to_internal;
         static constexpr auto inf = std::numeric_limits<float>::infinity();
+
+        inline namespace shade_path_details
+        {
+            inline namespace shade_missed
+            {
+                linear_rgb shade_path(
+                        hits::missed const& /*miss*/,
+                        scene::materials::solid_color const& mat,
+                        lamp_container_type const& /*lamps*/)
+                {
+                    return mat.color;
+                }
+
+                linear_rgb shade_path(
+                        hits::missed const& miss,
+                        scene::materials::phong const& mat,
+                        lamp_container_type const& lamps)
+                {
+                    throw std::runtime_error{"Environment cannot have a phong material."};
+                }
+
+                linear_rgb shade_path(
+                        hits::missed const& miss,
+                        scene::materials::physically_based const& mat,
+                        lamp_container_type const& lamps)
+                {
+                    throw std::runtime_error{"Environment cannot have a PBR material."};
+                }
+            }
+
+            inline namespace shade_hit_object
+            {
+                linear_rgb shade_path(
+                        hits::object const& /*hit*/,
+                        scene::materials::solid_color const& mat,
+                        lamp_container_type const& /*lamps*/,
+                        linear_rgb const& /*shaded*/)
+                {
+                    return mat.color;
+                }
+
+                linear_rgb shade_path(
+                        hits::object const& hit,
+                        scene::materials::phong const& mat,
+                        lamp_container_type const& lamps,
+                        linear_rgb const& shaded)
+                {
+                    if (lamps.empty()) {    // This is only for Assignment 1 compatibility
+                        return mat.diffuse;
+                    } else {
+                        throw std::runtime_error{"TODO"};
+                    }
+                }
+
+                linear_rgb shade_path(
+                        hits::object const& hit,
+                        scene::materials::physically_based const& mat,
+                        lamp_container_type const& lamps,
+                        linear_rgb const& shaded)
+                {
+                    throw std::runtime_error{"TODO"};
+                }
+            }
+        }
     }
 
     image_type shade(path_buffer_type const& buf, scene_type const& scene)
     {
-        image_type img{buf.size(), linear_rgb{0.8, 0.4, 0.1}};
+        image_type img{buf.size()};
+        auto& env_mat = scene.materials[scene.environment];
+        auto& lamps = scene.lamps;
+
+        img.each([&] (auto& pixel, auto pos) {
+            auto& hits = buf[pos].hits;
+            auto& miss = buf[pos].miss;
+
+            pixel = env_mat.match(
+                    [&] (auto& mat) {
+                        return shade_path(miss, mat, lamps);
+                    });
+
+            for (auto& hit: hits) {
+                auto& mat = scene.materials[hit.material_id];
+                pixel = mat.match(
+                        [&] (auto& mat) {
+                            return shade_path(hit, mat, lamps, pixel);
+                        });
+            }
+        });
         return img;
     }
 
