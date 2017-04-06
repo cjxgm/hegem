1d60e682091a11a6ae252277a1a4e4ff3a59b3b7
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-04-07 02:12:43 +0800

    [+] More test scenes
    - [+] PBR and phong scenes, day version
    - [+] Sunny scene
    - [+] Triple moons scene

--- build.pl
+++ build.pl
@@ -18,25 +18,23 @@ $build__output_bin = "raytracer";
 
 my $size = [16*60, 9*60];
 my @inputs = (
-    # [ input-name, size, depth-range ]
-    #   size = [width, height]
-    #   depth-range = [min, max]
-    [ 'scene-phong-night', $size, [2, 6] ],
-    [ 'scene-pbr-night', $size, [2, 6] ],
+    [ 'scene-phong-night', $size ],
+    [ 'scene-pbr-night', $size ],
+    [ 'scene-phong-day', $size ],
+    [ 'scene-pbr-day', $size ],
+    [ 'scene-sunny', $size ],
+    [ 'scene-triple-moon', $size ],
 );
 @makefile__commands__test = (
     (map {
-        my ($name, $size, $depth_range) = @$_;
+        my ($name, $size) = @$_;
         my ($width, $height) = @$size;
-        my ($depth_min, $depth_max) = @$depth_range;
 
         my $output = "\$(BUILD)/output-$name-${width}x$height.png";
-        my $normal_output = "\$(BUILD)/output-$name-${width}x$height-normal.png";
-        my $depth_output = "\$(BUILD)/output-$name-${width}x$height-depth.png";
         {
             name => $name,
             commands => [
-                "\$(BIN) --input support/inputs/$name.txt --width $width --height $height --output $output --normal $normal_output --depth $depth_output --depth-min $depth_min --depth-max $depth_max",
+                "\$(BIN) --input support/inputs/$name.txt --width $width --height $height --output $output",
             ],
         }
     } @inputs),
@@ -44,13 +42,11 @@ my @inputs = (
         name => "display result",
         commands => [
             ("feh " . join " ", map {
-                my ($name, $size, $depth_range) = @$_;
+                my ($name, $size) = @$_;
                 my ($width, $height) = @$size;
 
                 my $output = "\$(BUILD)/output-$name-${width}x$height.png";
-                my $normal_output = "\$(BUILD)/output-$name-${width}x$height-normal.png";
-                my $depth_output = "\$(BUILD)/output-$name-${width}x$height-depth.png";
-                ($output, $normal_output, $depth_output)
+                ($output);
             } @inputs),
         ],
     },
