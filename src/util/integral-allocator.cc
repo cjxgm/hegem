8470325ff98088ae1efecf63d494a233055ce2d5
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-04-27 18:01:23 +0800

    [+] Add integral allocator
    An integral allocator allocates non-negative integer numbers consecutively.

---
+++ src/util/integral-allocator.cc
@@ -0,0 +1,26 @@
+#include "integral-allocator.hh"
+#include <cassert>
+
+namespace rt::util
+{
+    auto integral_allocator::allocate() -> value_type
+    {
+        auto x = free;
+        if (x == static_cast<value_type>(next.size()))
+            next.emplace_back(x+1);
+        assert(x < static_cast<value_type>(next.size()));
+
+        free = next[x];
+        return x;
+    }
+
+    void integral_allocator::deallocate(value_type x)
+    {
+        if (x == -1) return;
+        assert(x >= 0);
+        assert(x < static_cast<value_type>(next.size()));
+        next[x] = free;
+        free = x;
+    }
+}
+
---
+++ src/util/integral-allocator.hh
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <limits>
+
+namespace rt::util
+{
+    // Allocates integers from 0 to maximum value.
+    struct integral_allocator
+    {
+        using value_type = int;
+
+        value_type allocate();
+        void deallocate(value_type x);
+
+    private:
+        std::vector<value_type> next{};
+        value_type free{};
+    };
+}
+
