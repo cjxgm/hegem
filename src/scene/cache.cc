5b51831a1976bacdf748a014a711231231856213
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-07-16 17:18:00 +0800

    [+] Inject test mesh into scenes

--- src/scene/cache.cc
+++ src/scene/cache.cc
@@ -2,9 +2,26 @@
 #include "scene.hh"
 #include "node.hh"
 #include <deque>
+#include <cmath>
 
 namespace rt::scene::cache_details
 {
+    namespace
+    {
+        shapes::mesh build_test_mesh(int subdivision)
+        {
+            shapes::mesh m;
+            shapes::direction_type normal = glm::vec3{0.0f, 0.0f, 1.0f};
+            for (int i=0; i<subdivision; i++) {
+                float a = M_PI * 2 / subdivision * i;
+                m.verts.push_back({ glm::vec3{std::cos(a), std::sin(a), 0.0f}, normal });
+                m.faces.emplace_back(i, (i+1) % subdivision, subdivision);
+            }
+            m.verts.push_back({ glm::vec3{0.0f, 0.0f, 0.0f}, normal });
+            return m;
+        }
+    }
+
     cached_object::cached_object(material_id_type material_id, shape_type shape, glm::mat4 const& model_to_world)
         : material_id{material_id}
         , shape{std::move(shape)}
@@ -40,6 +57,16 @@ namespace rt::scene::cache_details
             pending_nodes.pop_front();
             pending_xforms.pop_front();
         }
+
+        // inject test mesh
+        auto test_mesh = build_test_mesh(37);
+        glm::mat4 test_xform{
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 3.0f, 1.0f,
+        };
+        sc.objects.emplace_back(0, std::move(test_mesh), std::move(test_xform));
 
         return sc;
     }
