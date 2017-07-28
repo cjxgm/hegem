a7b8ab598793decedc3c105ad83db8c259d9cf45
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-07-28 15:28:20 +0800

    [+] Check shader compilation and linking errors explicitly
    ...since `KHR_debug` on nvidia drivers doesn't report
    shader compilation and linking errors.

--- src/glu/shader.cc
+++ src/glu/shader.cc
@@ -10,6 +10,44 @@ namespace rt::glu::shader_factory
     {
         auto j() { return util::journal{"FACTORY"} << "\e[0;36m[SHADER]\e[0m "; }
 
+        void check_shader_error(gl::uint_type id, util::as_czstring path)
+        {
+            gl::int_type ok;
+            gl::get_shaderiv(id, gl::compile_status, &ok);
+            if (ok) return;
+
+            gl::int_type len;
+            gl::get_shaderiv(id, gl::info_log_length, &len);
+
+            std::string msg(len, '\0');
+            gl::get_shader_info_log(id, len, nullptr, &msg[0]);
+
+            std::string err{"Error when compiling shader "};
+            err += path.data();
+            err += ":\n";
+            err += msg;
+            throw std::runtime_error{err};
+        }
+
+        void check_program_error(gl::uint_type id, util::as_czstring name)
+        {
+            gl::int_type ok;
+            gl::get_programiv(id, gl::link_status, &ok);
+            if (ok) return;
+
+            gl::int_type len;
+            gl::get_programiv(id, gl::info_log_length, &len);
+
+            std::string msg(len, '\0');
+            gl::get_program_info_log(id, len, nullptr, &msg[0]);
+
+            std::string err{"Error when linking program "};
+            err += name.data();
+            err += ":\n";
+            err += msg;
+            throw std::runtime_error{err};
+        }
+
         template <gl::enum_type Type>
         auto maybe_shader_from_name(util::as_czstring name) -> lib::optional<shared_shader<Type>>
         {
@@ -24,6 +62,7 @@ namespace rt::glu::shader_factory
                 int size = src.size();
                 gl::shader_source(s, 1, &data, &size);
                 gl::compile_shader(s);
+                check_shader_error(s, path);
                 return s;
             } else {
                 return {};
@@ -57,8 +96,18 @@ namespace rt::glu::shader_factory
     {
         j() << "\e[0;32m(" << name << ")\e[0m compiling program" << "\n";
         auto prog = program_pool::instance().allocate();
-        attach_all_available_shaders(prog, name);
-        gl::link_program(prog);
+        try {
+            attach_all_available_shaders(prog, name);
+            gl::link_program(prog);
+            check_program_error(prog, name);
+        }
+        catch (std::runtime_error const& ex) {
+            std::string msg{"Error while preparing program "};
+            msg += name.data();
+            msg += ":\n";
+            msg += ex.what();
+            throw std::runtime_error(msg);
+        }
         return prog;
     }
 }
