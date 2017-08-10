f876ff1f49b23a4754444d005db40d466463f901
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-08-10 18:23:53 +0800

    [*] Fix unclamped hotness in voxel visualization shader

--- support/shaders/g-box/vert.glsl
+++ support/shaders/g-box/vert.glsl
@@ -9,11 +9,12 @@ out vert_geom
 next;
 
 const vec4 cold_color = vec4(7.0f, 8.0f, 10.0f, 0.005);
-const vec4 hot_color = vec4(10.0f, 4.0f, 1.0f, 0.008);
+const vec4 warm_color = vec4(10.0f, 4.0f, 1.0f, 0.01);
+const vec4 hot_color = vec4(10.0f, 0.0f, 0.0f, 0.2);
 
 void main()
 {
     gl_Position = vec4(0, 0, 0, 1);
-    next.color = mix(cold_color, hot_color, hotness);
+    next.color = mix(mix(cold_color, warm_color, clamp(hotness / 5.0f, 0.0f, 1.0f)), hot_color, clamp(hotness / 10.0f, 0.0f, 1.0f));
 }
 
