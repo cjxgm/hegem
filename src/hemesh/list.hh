1eea581ff7f91632cb7a8486a937fec80a6819ae
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-11-03 01:43:12 +0800

    [+] hemesh: Iterate through lists with priters

--- src/hemesh/list.hh
+++ src/hemesh/list.hh
@@ -1,4 +1,6 @@
 #pragma once
+// Operators for generic doubly-linked cyclic lists.
+#include "../util/priter.hh"
 
 namespace rt::hemesh
 {
@@ -29,6 +31,37 @@ namespace rt::hemesh
         {
             if (!a) a = x;
             return insert_before(a, x);
+        }
+
+        template <class T>
+        struct priter
+        {
+            using value_type = T;
+
+            priter() = default;
+            priter(value_type* first) : first{first}, now{first} {}
+
+            value_type* operator ++ ()
+            {
+                if (now == nullptr)
+                    return nullptr;
+
+                auto result = now;
+                now = now->next;
+                if (now == first)
+                    now = nullptr;
+                return result;
+            }
+
+        private:
+            value_type* first;
+            value_type* now;
+        };
+
+        template <class T>
+        auto iterate(T* first)
+        {
+            return util::range_from_priter{priter{first}};
         }
     }
 }
