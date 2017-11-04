fb2e1870b99e76988b8790d4799cbabfed23d380
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-11-05 00:47:22 +0800

    [+] hemesh: Add geometric utilities

---
+++ src/hemesh/geometry.cc
@@ -0,0 +1,54 @@
+#include "../lib/glm/op/geom.hh"
+#include "geometry.hh"
+#include "primitive.hh"
+#include "list.hh"
+
+namespace rt::hemesh
+{
+    inline namespace geometry_operator
+    {
+        namespace
+        {
+            position_type yzx(position_type a) { return { a.y, a.z, a.x }; }
+        }
+
+        direction_type normal(hege_type* hege, float eps, direction_type fallback)
+        {
+            // Use Newell's method to calculate the normal.
+            // It works for concave polygons and near-parallel edges.
+            //
+            // Tampieri, Filippo. 1992. "Newell's Method for Computing
+            // the Plane Equation.". Graphics Gems III 231–232. Academic Press.
+            offset_type normal;
+            for (auto& h: list::iterate(hege)) {
+                auto u = h.      start->pos;
+                auto v = h.next->start->pos;
+                auto u0 = yzx(u);
+                auto v0 = yzx(v);
+                auto u1 = yzx(u0);
+                auto v1 = yzx(v0);
+                normal += (u0 - v0) * (u1 + v1);
+            }
+
+            if (dot(normal, normal) <= eps*eps) return fallback;
+            return normal;
+        }
+
+        bool is_same_side(direction_type dir0, direction_type dir1, float eps)
+        {
+            auto sideness = dot(*dir0, *dir1);
+            return (sideness > eps);
+        }
+
+        bool is_same_side(
+            direction_type normal, position_type origin,    // a plane
+            position_type point,
+            float eps)
+        {
+            auto offset = point - origin;
+            if (dot(offset, offset) <= eps) return false;
+            return is_same_side(normal, offset, eps);
+        }
+    }
+}
+
--- src/hemesh/type.hh
+++ src/hemesh/type.hh
@@ -1,9 +1,12 @@
 #pragma once
 // basic types
 #include "../lib/glm/vec3.hh"
+#include "../math/direction.hh"
 
 namespace rt::hemesh
 {
     using position_type = glm::vec3;
+    using offset_type = glm::vec3;
+    using direction_type = math::direction_type;
 }
 
