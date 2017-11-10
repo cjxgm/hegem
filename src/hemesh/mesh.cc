ad0be0a2c0ed13bd57766f224cadcc8fa68c76f1
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-11-10 15:29:26 +0800

    [+] hemesh: Visualize half-edges
    - Half-edge to mesh conversion
    - Add the half-edge mesh into the example scene

--- src/hemesh/mesh.cc
+++ src/hemesh/mesh.cc
@@ -61,6 +61,63 @@ namespace rt::hemesh
 
             return tri_mesh;
         }
+
+        scene::shapes::mesh outline_hege(hege_type* hege, direction_type normal)
+        {
+            constexpr auto width = 0.02f;
+            constexpr auto arrow_center_sliding = 0.52f;
+            constexpr auto arrow_head_length = 0.10f;
+            constexpr auto arrow_tail_length = 0.01f;
+            constexpr auto arrow_width = 0.05f;
+
+            scene::shapes::mesh tri_mesh;
+            auto p0 = hege->start->pos;
+            auto p1 = hege->twin->start->pos;
+
+            if (distance(p0, p1) < 0.2f) return tri_mesh;
+
+            // Build local axis
+            //      z: normal
+            //      y: the same direction as hege
+            //      x: cross(y, z)
+            auto y = direction_type{p1 - p0};
+            auto x = direction_type{cross(*y, *normal)};
+
+            // hege
+            tri_mesh.verts.push_back({ p0,             normal });   // 0
+            tri_mesh.verts.push_back({ p1,             normal });   // 1
+            tri_mesh.verts.push_back({ p0 - x * width, normal });   // 2
+            tri_mesh.verts.push_back({ p1 - x * width, normal });   // 3
+            tri_mesh.faces.emplace_back(0, 1, 2);
+            tri_mesh.faces.emplace_back(3, 2, 1);
+
+            // arrow
+            auto arrow_center = mix(p0, p1, arrow_center_sliding) - x * width;
+            auto arrow_head = arrow_center + y * arrow_head_length;
+            auto arrow_tail = arrow_center - y * arrow_tail_length - x * arrow_width;
+            tri_mesh.verts.push_back({ arrow_center, normal });     // 4
+            tri_mesh.verts.push_back({ arrow_head,   normal });     // 5
+            tri_mesh.verts.push_back({ arrow_tail,   normal });     // 6
+            tri_mesh.faces.emplace_back(4, 5, 6);
+
+            return tri_mesh;
+        }
+
+        scene::shapes::mesh outline_face(face_type* face, float bias)
+        {
+            scene::shapes::mesh tri_mesh;
+            auto n = normal(face->boundary->any_hege);
+            auto offset = n * bias;
+
+            for (auto& r: iterate(face->boundary))
+                for (auto& h: iterate(r.any_hege))
+                    extend(tri_mesh, outline_hege(&h, n));
+
+            for (auto& v: tri_mesh.verts)
+                v.position += offset;
+
+            return tri_mesh;
+        }
     }
 
     scene::shapes::mesh build_mesh(hemesh const& m)
@@ -69,6 +126,17 @@ namespace rt::hemesh
         for (auto& b: iterate(m.any_body)) {
             for (auto& f: iterate(b.any_face)) {
                 extend(tri_mesh, triangulate_face(&f));
+            }
+        }
+        return tri_mesh;
+    }
+
+    scene::shapes::mesh build_outline_mesh(hemesh const& m, float bias)
+    {
+        scene::shapes::mesh tri_mesh;
+        for (auto& b: iterate(m.any_body)) {
+            for (auto& f: iterate(b.any_face)) {
+                extend(tri_mesh, outline_face(&f, bias));
             }
         }
         return tri_mesh;
