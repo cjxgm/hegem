84bd35fbea0298cafd1cdd8746cc03b3e3638de1
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-11-11 17:48:25 +0800

    [@] hegem.op.shape: Polygons can be made as holes of faces

--- src/hegem/op/shape.hh
+++ src/hegem/op/shape.hh
@@ -13,8 +13,19 @@ namespace rt::hegem
         {
             inline namespace _2d
             {
-                hege_type* make_polygon(hemesh & m, util::const_span<position_type> points);
-                hege_type* make_polygon_disk(hemesh & m, int ngon, float radius);
+                // make_polygon(m, p, nullptr, nullptr)
+                //      Make a new body with a 2-sided polygon
+                //
+                // make_polygon(m, p, f, nullptr)
+                //      Make a polygon ring inside f
+                //      with a 1-sided face covering the ring
+                //
+                // make_polygon(m, p, f, cf)
+                //      Make a polygon ring inside f
+                //      with another ring inside cf,
+                //      so that the 2 new rings become a hole in f and cf.
+                hege_type* make_polygon(hemesh & m, util::const_span<position_type> points, face_type* outer_face=nullptr, face_type* counter_face=nullptr);
+                hege_type* make_polygon_disk(hemesh & m, int ngon, float radius, face_type* outer_face=nullptr, face_type* counter_face=nullptr);
             }
 
             inline namespace _3d
