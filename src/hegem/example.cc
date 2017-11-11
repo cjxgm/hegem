2efd122d067403660899858c41ab8fbac83564fb
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-11-11 22:32:14 +0800

    [@] hegem: Example with holes

--- src/hegem/example.cc
+++ src/hegem/example.cc
@@ -15,14 +15,39 @@ namespace rt::hegem
         {
             rt::hegem::hemesh m;
 
-            auto cube = make_cube(m)->ring->face->body;
-            auto cylinder = make_polygon_cylinder(m, 12, 1.0f, 4, 2.0f)->ring->face->body;
+            auto cube_hege = make_cube(m);
+            auto cube = cube_hege->ring->face->body;
+            auto cube_top = cube_hege->next->next->twin->ring->face;
+            {
+                auto hole = make_polygon_disk(m, 6, 0.2f, cube_top)->ring;
+                affine_transform(hole, glm::translate(glm::vec3{ 0.0f, 1.0f, 0.0f }));
+                extrude(m, hole->face, { 0.0f, 0.5f, 0.0f });
+            }
+
+            auto cylinder_face = make_polygon_disk(m, 12, 1.0f)->ring->face;
+            auto cylinder_counter_face = cylinder_face->boundary->any_hege->twin->ring->face;
+            auto cylinder = cylinder_face->body;
+            {
+                auto hole0 = make_polygon_disk(m, 4, 0.2f, cylinder_face, cylinder_counter_face)->ring;
+                auto hole1 = make_polygon_disk(m, 6, 0.5f, cylinder_face, cylinder_counter_face)->ring;
+                hole1->any_hege->next->next->next->start->pos = {};
+                affine_transform(hole0, glm::translate(glm::vec3{ -0.3f, 0.0f, 0.0f }));
+                affine_transform(hole1, glm::translate(glm::vec3{ 0.3f, 0.0f, 0.0f }));
+            }
+            extrude(m, cylinder_face, {  0.15f, 0.5f, 0.0f });
+            extrude(m, cylinder_face, {  0.05f, 0.5f, 0.0f });
+            extrude(m, cylinder_face, { -0.05f, 0.5f, 0.0f });
+            extrude(m, cylinder_face, { -0.15f, 0.5f, 0.0f });
 
             auto disk_face = make_polygon_disk(m, 12, 1.0f)->ring->face;
             auto disk_counter_face = disk_face->boundary->any_hege->twin->ring->face;
             auto disk = disk_face->body;
-            auto hole0 = make_polygon_disk(m, 4, 0.2f, disk_face, disk_counter_face)->ring;
-            auto hole1 = make_polygon_disk(m, 5, 0.3f, disk_face, disk_counter_face)->ring;
+            {
+                auto hole0 = make_polygon_disk(m, 4, 0.2f, disk_face, disk_counter_face)->ring;
+                auto hole1 = make_polygon_disk(m, 5, 0.3f, disk_face, disk_counter_face)->ring;
+                affine_transform(hole0, glm::translate(glm::vec3{ -0.3f, 0.0f, 0.0f }));
+                affine_transform(hole1, glm::translate(glm::vec3{ 0.3f, 0.0f, 0.0f }));
+            }
 
             // ">>" shape (it's actually "<<" shape)
             //  _______
@@ -40,10 +65,8 @@ namespace rt::hegem
             affine_transform(cube, glm::translate(glm::vec3{ 0.0f, 0.0f, -2.0f }));
             affine_transform(arrow, glm::translate(glm::vec3{ -2.0f, 0.0f, 0.0f }));
             affine_transform(cylinder, glm::translate(glm::vec3{ 0.0f, 0.5f, 0.0f }));
-            affine_transform(hole0, glm::translate(glm::vec3{ -0.3f, 0.0f, 0.0f }));
-            affine_transform(hole1, glm::translate(glm::vec3{ 0.3f, 0.0f, 0.0f }));
-            affine_transform(disk, glm::translate(glm::vec3{ 2.0f, 1.0f, 0.0f }));
-            affine_transform_all(cube, glm::translate(glm::vec3{ 0.0f, -1.0f, 1.0f }));
+            affine_transform(disk, glm::translate(glm::vec3{ 2.5f, 1.0f, 0.0f }));
+            affine_transform_all(cube, glm::translate(glm::vec3{ 0.0f, -1.0f, 0.0f }));
 
             auto tm = build_mesh(m);
             write_obj(tm, std::cerr);
