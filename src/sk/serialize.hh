ed5ac1b0371170c0460bfeef8488ee061388434e
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2017-12-13 13:35:16 +0800

    [+] sk: Add serialize() for saving and dumping

---
+++ src/sk/serialize.hh
@@ -0,0 +1,32 @@
+#pragma once
+#include "graph.hh"
+
+namespace rt::sk
+{
+    template <class Serializer>
+    void serialize(Serializer& s, graph& g, node_id_type previewing_node)
+    {
+        g.collect_garbage();
+
+        for (auto& n: g.node_range()) {
+            auto& instance = n.instance;
+            auto previewing = (n.id == previewing_node);
+
+            {
+                auto fs = s.node(n.metadata->id, n.x, n.y, n.width, previewing);
+                switch (instance.id) {
+                    #define OP(KIND, ID, ARITY, NAME, TOOLTIP, FIELDS...) \
+                        case op_id::KIND##_##ID: { \
+                            auto& fields = instance.fields.KIND##_##ID; \
+                            (void)fields; \
+                            FIELDS \
+                        } break;
+                    #define FIELD(TYPE, VAR, INITIAL, WIDGET, ...) \
+                        fs.WIDGET(#VAR, fields.VAR);
+                    #include "op.inl"
+                }
+            }
+        }
+    }
+}
+
