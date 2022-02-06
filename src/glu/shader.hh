09542ef72b960c9cdc3718a9524752ec90678c92
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2022-02-06 15:18:07 +0800

    glu: Support injecting code into shaders

--- src/glu/shader.hh
+++ src/glu/shader.hh
@@ -3,12 +3,13 @@
 #include "../lib/std/optional.hh"
 #include "../util/as-czstring.hh"
 #include "resource.hh"
+#include <string>
 
 namespace rt::glu
 {
     namespace shader_factory
     {
-        shared_program program_from_name(util::as_czstring name);
+        shared_program program_from_name(util::as_czstring name, std::string const& injection={});
     }
 }
 
