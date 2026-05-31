c6a3cdb12c4a86f6ba2fd4764626cfdda43b8cee
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2026-05-31 15:15:17 +0800

    morpha: Revive the morphing editor
    This reverts commit 1aca3a821110d6f0ff1b2fad8d50f43278801f2c.

---
+++ src/morpha/file-slot.cc
@@ -0,0 +1,55 @@
+#include "../util/file-dialog.hh"
+#include "../lib/glm/op/common.hh"
+#include "../lib/imgui.hh"
+#include "file-slot.hh"
+
+namespace rt::morpha
+{
+    struct file_slot::temporary_state
+    {
+        util::file_dialog dialog;
+        std::string filename;
+        std::string empty_string;
+    };
+
+    file_slot::file_slot(): tmp{std::make_unique<temporary_state>()} {}
+    file_slot::~file_slot() = default;
+
+    auto file_slot::draw(char const* title, char const* directory) -> std::string const&
+    {
+        auto area_width = ImGui::GetContentRegionAvailWidth();
+        auto btn_width = glm::min(area_width * 0.9f, 60.0f);
+
+        ImGui::PushID(title);
+        ImGui::Columns(2, title, false);
+        ImGui::SetColumnWidth(0, btn_width);
+        if (tmp->dialog) {
+            ImGui::AlignTextToFramePadding();
+            ImGui::TextDisabled("%s", "    ...");
+        } else {
+            if (ImGui::Button("Load")) {
+                tmp->dialog.open(
+                    util::file_dialog::action::open,
+                    title,
+                    directory);
+            }
+        }
+        ImGui::NextColumn();
+        ImGui::AlignTextToFramePadding();
+        if (tmp->filename.empty()) {
+            ImGui::TextDisabled("(no file loaded as %s)", title);
+        } else {
+            ImGui::Text("%s", tmp->filename.data());
+        }
+        ImGui::Columns();
+        ImGui::PopID();
+
+        if (auto opt_filename = tmp->dialog()) {
+            tmp->filename = std::move(*opt_filename);
+            return tmp->filename;
+        } else {
+            return tmp->empty_string;
+        }
+    }
+}
+
